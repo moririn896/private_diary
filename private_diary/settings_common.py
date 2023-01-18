ec09dae098e2537e1bf8edcb80abd80fca75cf65
moririn896 <moririn659@gmail.com>
2023-01-18 14:44:56 +0900

    コミット4

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -213,3 +213,8 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
 
 # デフォルトのメール送信元を設定
 DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL')
+
+MEDIA_URL = '/media/'
+
+# ログイン/ログアウト後の遷移先を設定
+LOGIN_REDIRECT_URL = 'diary:diary_list'
