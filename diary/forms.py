71931aeb0db66ab2ce93b13a01366e5b9fe412d5
moririn896 <moririn659@gmail.com>
2023-01-18 21:40:45 +0900

    コミット9

--- diary/forms.py
+++ diary/forms.py
@@ -56,4 +56,3 @@ class DiaryCreateForm(forms.ModelForm):
             super().__init__(*args, **kwargs)
             for field in self.fields.values():
                 field.widget.attrs['class'] = 'form-control'
-
